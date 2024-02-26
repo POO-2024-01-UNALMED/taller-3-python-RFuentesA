95002297036e4f0a15dcf9cedf32c22886d4cf5c
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-02-26 17:21:03 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -9,10 +9,10 @@ class Control:
         self._tv.canalUp()
     def canalDown(self):
         self._tv.canalDown()
-    def volumeUp(self):
-        self._tv.volumeUp()
-    def volumeDown(self):
-        self._tv.volumeDown()
+    def volumenUp(self):
+        self._tv.volumenUp()
+    def volumenDown(self):
+        self._tv.volumenDown()
     def setCanal(self, canal):
         self._tv.setCanal(canal)
     def setVolumen(self, volumen):
